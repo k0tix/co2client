ba86f26565497772998cb0eafba244d690475c4d
k0tix <otto.pirttimaa@gmail.com>
2019-03-07 15:24:38 +0200

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Segment, Container } from 'semantic-ui-react';
-
+import { Segment, Container, Grid, Icon } from 'semantic-ui-react';
 
 const Footer = () => {
+    const date = new Date()
     return (
-        <Segment inverted vertical style={{padding: '5em 0em', marginTop: '10em'}}>
-            <Container text>
-                this is a footer
-            </Container>
+        <Segment inverted vertical style={{padding: '5em 0em', marginTop: '24em'}}>
+            <Grid centered columns={2}>
+                <Grid.Column width={3}>
+                    <Grid.Row>
+                        <a href='https://github.com/k0tix/co2server' style={{color: 'white'}}>
+                        <Icon name='github' size='huge'></Icon>
+                        made by Otto Pirttimaa
+                    </a>
+                    </Grid.Row>
+                </Grid.Column>
+            </Grid>
         </Segment>
     )
 }
