aa9492f55875eeb0b3f1ce34347e384ba2d08ab9
k0tix <otto.pirttimaa@gmail.com>
2019-03-07 15:29:21 +0200

    Remove per capita toggle

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {searchCountry} from '../reducers/countryReducer'
+import { searchCountry } from '../reducers/countryReducer'
 import { togglePerCapita, updateSearchText } from '../reducers/searchReducer'
-import { Item, Icon, Form, Checkbox, Grid, Divider, Label, Segment, Search, Container } from 'semantic-ui-react'
-
-import EmissionChart from '../components/EmissionChart'
-
-import data from '../test'
+import { Item, Icon, Form, Grid, Search } from 'semantic-ui-react'
 
 class SearchBar extends React.Component {
 
@@ -19,27 +15,6 @@ class SearchBar extends React.Component {
         this.props.searchCountry(this.props.searchText)
     }
 
-    togglePerCapita = () => {
-        this.props.togglePerCapita()
-    }
-
-    processdata = (values) => {
-        /*var year = 1960
-        var emissions = new Array(['Year', 'Emissions'])
-        values.forEach(value => {
-            emissions.push([`${year}`, value])
-            year++
-        }
-        )
-
-        return emissions*/
-        var emissions = new Array(['Year', 'Emission'])
-        values.forEach(value => {
-            emissions.push([value.year, parseInt(value.value)])
-        })
-        return emissions
-    }
-
     handleResultSelect = (event, { result }) => {
         this.props.updateSearchText(result.name)
         this.props.searchCountry(result.code)
@@ -50,12 +25,11 @@ class SearchBar extends React.Component {
             <Grid centered>
                 <Grid.Column style={{ maxWidth: '600px' }}>
                     <Form size='large' onSubmit={this.props.handleSearch} >
-                        <Segment>
                             <Form.Field style={{ padding: '1em' }}>
                             <Search fluid
-                                size='huge'
+                                size='big'
                                 icon={<Icon name='search' inverted circular link color='blue' />}
-                                placeholder='country name...'
+                                placeholder='Country name...'
                                 value={this.props.searchText}
                                 onSearchChange={this.handleSearchTextChange}
                                 onResultSelect={this.handleResultSelect}
@@ -64,41 +38,17 @@ class SearchBar extends React.Component {
                                 minCharacters={2}
                                 results={this.props.filteredResults}
                             />
-                        </Form.Field>
-    
-                        <Form.Group inline style={{ margin: '1em' }}>
-                            <Form.Field>
-                                <Checkbox toggle label='per capita' onChange={this.togglePerCapita} onClick={() => console.log(this.props)}/>
-                            </Form.Field>
-                        </Form.Group>
-                        </Segment>
-                        
-                    </Form>
-    
-                    <Divider horizontal>OR</Divider>
-                    <Segment textAlign='center'>
-                        <Label color='pink' size='big'>Choose a country from a map</Label>
-                    </Segment>
-
-            <Container fluid>
-
-            
-                    {this.props.country.emissions === undefined ? null : 
-                        <EmissionChart name={this.props.country.name} id={this.props.country.code} values={this.processdata(this.props.country.emissions)}>
-                        </EmissionChart>
-
-                    }
-               </Container>                             
+                        </Form.Field>                     
+                    </Form>                     
                 </Grid.Column>
-
-                    
             </Grid>
         )
     }
-}
+} 
 
 const renderResult = ({name}) => (
-    <Item key={name}>{name}</Item>
+    <Item key={name}>{name}
+    </Item>
 )
 
 const filterResults = (results, text) => {
